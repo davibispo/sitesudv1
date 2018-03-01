ac9973faa12d51935dc693ebab55ce8a06e85a29
davibispo <davi-bispo@hotmail.com>
2018-03-01 16:25:42 -0300

    sair

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -158,7 +158,7 @@
                                            onclick="event.preventDefault();
                                                    document.getElementById('logout-form').submit();">
                                             <i class="fas fa-sign-out-alt"></i> 
-                                            Logout
+                                            SAIR
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
