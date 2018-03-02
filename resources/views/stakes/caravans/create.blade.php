637993182228513130c7e71b85dbd6f61c7d3b0f
davibispo <davi-bispo@hotmail.com>
2018-03-02 09:18:05 -0300

    corrigindo campos de valores do criar caravana

--- resources/views/stakes/caravans/create.blade.php
+++ resources/views/stakes/caravans/create.blade.php
@@ -35,13 +35,13 @@
                     <div class="form-group">
                         {!! Form::label('bus', 'Aluguel do Ônibus (valor total)', ['class'=>'col-md-4 control-label']) !!}
                         <div class="col-md-6">
-                            {!! Form::text('bus', null, ['class' => 'form-control', 'required']) !!}
+                            {!! Form::text('bus', null, ['class' => 'form-control', 'required', 'placeholder'=>'Não use ponto nem vírgula']) !!}
                         </div>
                     </div>
                     <div class="form-group">
                         {!! Form::label('accommodation', 'Alojamento (valor de 1 pernoite)', ['class'=>'col-md-4 control-label']) !!}
                         <div class="col-md-6">
-                            {!! Form::text('accommodation', null, ['class' => 'form-control', 'required']) !!}
+                            {!! Form::text('accommodation', null, ['class' => 'form-control', 'required','placeholder'=>'Não use ponto nem vírgula']) !!}
                         </div>
                     </div>
                     <div class="form-group">
