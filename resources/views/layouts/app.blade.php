25f55e73f5038a445c6934dde91a639b93d285ad
davibispo <davi-bispo@hotmail.com>
2018-03-08 08:44:05 -0300

    link criar/editar caravanas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
                                     <li><a href="{{route('caravan-users.index')}}">Reservas</a></li>
                                     @endcan
                                     @can('caravan_index')
-                                    <li><a href="{{route('caravans.index')}}">Criar</a></li>
+                                    <li><a href="{{route('caravans.index')}}">Criar/editar</a></li>
                                     @endcan
                                 </ul>
                             </li> 
