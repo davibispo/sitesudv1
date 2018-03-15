015cdec21ad71b85e3a684dabc4df4558569ebfe
davibispo <davi-bispo@hotmail.com>
2018-03-15 10:15:26 -0300

    redirect para /login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,6 +19,8 @@ class RedirectIfAuthenticated
     {
         if (Auth::guard($guard)->check()) {
             return redirect('/');
+        }else{
+            return redirect('/login');
         }
 
         return $next($request);
