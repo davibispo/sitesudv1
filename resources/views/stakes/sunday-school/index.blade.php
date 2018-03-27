12ddcfa21cf62380ff2c0400070030830da119c5
davibispo <davi-bispo@hotmail.com>
2018-03-27 16:58:03 -0300

    acrescentando MAS

---
+++ resources/views/stakes/sunday-school/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Calendário da Estaca ...</b></div>
+
+                <div class="panel-body">
+                    
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>Data</th>
+                                <th>Doutrina do Evangelho</th>
+                                <th>Jovens</th>
+                                <th>Quórum/Sumo/Soc.Socorro</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                
+                            <div class=" well alert-warning">
+                                <p>Não tem aulas cadastrados!</p>
+                            </div>
+                           
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
+
