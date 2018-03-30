f01ae12806b05d125f6187e7169510ced09eb62e
davibispo <davi-bispo@hotmail.com>
2018-03-29 23:42:02 -0300

    Agendas sacramentais, melhorias

--- resources/views/stakes/sacramental-meetings/index.blade.php
+++ resources/views/stakes/sacramental-meetings/index.blade.php
@@ -6,22 +6,27 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading"><b>Agendas de Reunião Sacramental de {{strtoupper($ward)}} da Estaca {{$stake}}</b></div>
+                <div class="panel-heading"><b>Agendas Sacramentais</b></div>
 
                 <div class="panel-body">
+                    @can('sacramental_meeting_create')
                     <div class="">
                         <a href="{{route('sacramental-meetings.create')}}" class="btn btn-primary btn-sm">
                             <i class="fas fa-plus"></i> 
                             Nova Agenda
                         </a>
                     </div>
+                    @endcan
                     <br>    
-                    <table class="table table-striped">
+                    <table class="table table-striped table-hover">
                         <tr>
                             <th>Data</th>
                             <th>Presidida</th>
                             <th>Dirigida</th>
                             <th>Frequência</th>
+                            @can('sacramentais_stake')
+                                <th>Unidade</th>
+                            @endcan
                             <th></th>
                         </tr>
                         @foreach ($sacramentalMeetings as $sacramentalMeeting)
@@ -30,12 +35,17 @@
                                 <td>{{$sacramentalMeeting->presidida}}</td>
                                 <td>{{$sacramentalMeeting->dirigida}}</td>
                                 <td>{{$sacramentalMeeting->frequencia}}</td>
+                                @can('sacramentais_stake')
+                                    <td>{{$sacramentalMeeting->ward}}</td>
+                                @endcan
                                 <td>
+                                    @can('sacramental_meeting_update')
                                     <a href="{{route('sacramental-meetings.edit',$sacramentalMeeting->id)}}" class="btn btn-warning btn-xs"><i class="fas fa-edit"></i> editar</a>
                                     <a href="{{route('sacramental-meetings.show',$sacramentalMeeting->id)}}" target="_blank" class="btn btn-success btn-xs"><i class="fas fa-print"></i> imprimir</a>
                                     {!! Form::open(['method'=>'DELETE', 'action'=>['SacramentalMeetingController@destroy', $sacramentalMeeting->id], 'style'=>'display:inline']) !!}
                                         {!! Form::submit('excluir', ['class'=>'btn btn-danger btn-xs']) !!}
                                     {!! Form::close() !!}
+                                    @endcan
                                 </td>
                             </tr>
                         @endforeach
