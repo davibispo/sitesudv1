92cbd5c5a0ecdf54e4714ad050c445acc11050d8
davibispo <davi-bispo@hotmail.com>
2018-04-11 17:05:56 -0300

    inclusão de dados do setenta de area

--- resources/views/conselor/index.blade.php
+++ resources/views/conselor/index.blade.php
@@ -11,21 +11,27 @@
                 </div>
 
                 <div class="panel-body">
-                    
-                    <div class="well text-center">
-                        Esta página é visualizada apenas pelos membros do Conselho de Coordenação Maceió.
+                    <div class="text-center">
+                        <img src="{!! asset('assets/img/elder-simplicio.jpeg') !!}" class="img-thumbnail" style="width:15%">
+                        <div class="caption">
+                            <p>
+                                <b>Élder</b> Henrique Sérgio <b>Simplício</b><br>
+                                (85) 9603-2240<br>
+                                simpliciohsa@ldschurch.org
+                            </p>
+                        </div>
                     </div>
-                    
-                    <div class="btn-group btn-group-justified">
+
+                    <div class="btn-group btn-group-justified" style="height:60px">
                         <a href="{{route('conselor.calendars.index')}}" class="btn btn-default btn-lg"><i class="fas fa-calendar-alt"></i> Calendário</a>
                         <a href="{{route('conselor.stakes.index')}}" class="btn btn-default btn-lg"><i class="fas fa-home"></i> Estacas/Distritos</a>
                         <a href="{{route('conselor.leaders.index')}}" class="btn btn-default btn-lg"><i class="fas fa-users"></i> Especialistas</a>
                     </div>
-                    
-                    <div class="btn-group btn-group-justified">
+
+                    <div class="btn-group btn-group-justified" style="height:60px">
                         <a href="{{route('conselor.meetings.index')}}" class="btn btn-default btn-lg"><i class="fas fa-edit"></i> Agendas</a>
                         @can('adm')
-                        <a href="#" class="btn btn-default btn-lg"><i class="fas fa-copy"></i> Arquivos</a>
+                        <a href="{{route('conselor.files.index')}}" class="btn btn-default btn-lg"><i class="fas fa-copy"></i> Arquivos</a>
                         <a href="#" class="btn btn-default btn-lg"></a>
                         @endcan
                     </div>
