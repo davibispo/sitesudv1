f8098f616c4b64299ac46b431da453bb4af13797
davibispo <davi-bispo@hotmail.com>
2019-05-16 22:33:53 -0300

    Ala Caetés criada em Estaca Tabuleiro.

--- resources/views/adm/users/create.blade.php
+++ resources/views/adm/users/create.blade.php
@@ -172,7 +172,7 @@
     $(document).ready(function () {
         //verificar
         $.ajax({
-            url: 'https://gist.githubusercontent.com/davibispo/280e4c58786d298dd21038a21ec55fdc/raw/b8d4c6ae4890d0e817fbb623cdf0d00bfe0ff13e/estacas-alas.json',
+            url: 'https://gist.githubusercontent.com/davibispo/280e4c58786d298dd21038a21ec55fdc/raw/6892f99d8662a8cd3d4dd943f3972b9a4fa11619/estacas-alas.json',
             type: 'GET',
             dataType: 'json',
             cache: true,
