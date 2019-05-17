042e71cccc32cd9a9a97db3bfe67ec0681cdb279
davibispo <davi-bispo@hotmail.com>
2019-05-17 11:47:50 -0300

    tentativa recuperação de senha 1

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+    'host' => env('MAIL_HOST', 'smtp.hostinger.com.br'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 465),
+    'port' => env('MAIL_PORT', 587),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'davibispo.sud@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'sitesud@meuacesso.site'),
         'name' => env('MAIL_FROM_NAME', 'SiteSUD'),
     ],
 
