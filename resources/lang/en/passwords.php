6921da85a25cca9580bcd54e4942dfc55c020ca2
davibispo <davi-bispo@hotmail.com>
2019-12-05 08:32:48 -0300

    Mensagens em português para redefinir senha.

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'password' => 'A senha deve ter pelo menos 6 caracteres e ser igual na confirmação.',
+    'reset' => 'Sua senha foi redefinida com sucesso!',
+    'sent' => 'Enviamos um e-mail com um link para redefinir sua senha!',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "Não encontramos usuário com este e-mail cadastrado.",
 
 ];
