655fb52f67620d26b9cc08180dc2a9404c941efb
davibispo <davi-bispo@hotmail.com>
2019-12-19 07:38:26 -0300

    anuncio_google_ADs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -171,6 +171,20 @@
             @yield('content')
 
             <div class="text-center">
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- anuncio1 -->
+                <ins class="adsbygoogle"
+                    style="display:block"
+                    data-ad-client="ca-pub-9352891206341061"
+                    data-ad-slot="1230677614"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
+
+            <div class="text-center">
                 <p></p>
                 <p>Copyright &#174 2018-{{date('Y')}} | by Davi Bispo - (82) 99969-3407 | davi-bispo@hotmail.com</p>
                 <p><i>(Este não é um site oficial de A Igreja de Jesus Cristo dos Santos dos Últimos Dias)</i></p>
