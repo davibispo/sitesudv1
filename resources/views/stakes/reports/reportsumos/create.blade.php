2e26e849e7fc25ef405b2da9fb3964fa06b7f61f
dvb <davi-bispo@hotmail.com>
2020-03-03 13:21:53 -0300

    continuando relatorio de sumo..

--- resources/views/stakes/reports/reportsumos/create.blade.php
+++ resources/views/stakes/reports/reportsumos/create.blade.php
@@ -18,25 +18,86 @@
                     <div class="alert alert-success">
                         <p>Texto de incentivo...</p>
                     </div>
-                    {!! Form::open([]) !!}
-                        <div class="input-group">
-                            <span class="input-group-addon">Sumo Conselheiro</span>
-                            <input type="text" class="form-control" name="nome" value="{{ $nome }} {{ $sobrenome }}" disabled>
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon">Unidade</span>
-                            <input type="text" class="form-control" name="unidade" value="{{ $unidade }}" disabled>
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon">Contato</span>
-                            <input type="text" class="form-control" name="telefone" value="{{ $telefone }}" disabled>
-                        </div>
+                    
+                    <div class="container">
+                        <form class="form-horizontal" action="/action_page.php">
 
-                        <div class="input-group">
-                            <span class="input-group-addon">Assistiu o Treinamento de Ministração</span>
-                            <input type="text" class="form-control" name="telefone" value="{{ $telefone }}">
-                        </div>
-                    {!! Form::close() !!}
+                          <div class="form-group">
+                            <label class="control-label col-sm-2" for="">Nome:</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="" placeholder="{{auth()->user()->name}} {{auth()->user()->lastname}} " disabled>
+                            </div>
+                          </div>
+
+                          <div class="form-group">
+                            <label class="control-label col-sm-2" for="">Responsável por:</label>
+                            <div class="col-sm-8">          
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Sociedade de Socorro
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Moças
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Rapazes
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Escola Dominical
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Primária
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Escola Dominical
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">JAS
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Assuntos Públicos
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">História da Família
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Caravanas
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Obra Missionária
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Seminário
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Tecnologia
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Música
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" value="">Auditoria
+                                </label>
+                            </div>
+                          </div>
+
+                          <div class="form-group">
+                            <label class="control-label col-sm-2" for="">Data de treinamento:</label>
+                            <div class="col-sm-3">
+                                <input type="date" class="form-control" id="">
+                                <p><i>Quando foi realizado treinamento</i></p>
+                            </div>
+                          </div>
+
+
+                          <div class="form-group">        
+                            <div class="col-sm-offset-2 col-sm-8">
+                              <button type="submit" class="btn btn-success">Enviar Formulário</button>
+                            </div>
+                          </div>
+
+                        </form>
+                    </div>
+                    
                 </div>
 
             </div>
